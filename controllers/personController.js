daaa3f3f682b9893e3dd88f8e4f505aac20edf02
Svetlana Zhilina <sveta199951@gmail.com>
2021-11-26 00:02:35 +0300

    feat: add func createPerson for POST method

--- controllers/personController.js
+++ controllers/personController.js
@@ -2,7 +2,7 @@ const Person = require('../models/personModel');
 
 //@desc Gets all persons
 //@route GET/person
-async function getPersons(res, res) {
+async function getPersons(req, res) {
   try {
     const person = await Person.findAll();
     res.writeHead(200, {'Content-Type':'text/html'});
@@ -14,7 +14,7 @@ async function getPersons(res, res) {
 
 //@desc Gets one person
 //@route GET/person/:personId
-async function getPerson(res, res, personUUID) {
+async function getPerson(req, res, personUUID) {
   try {
     const person = await Person.findByUUID(personUUID);
     if (!person){
@@ -29,4 +29,36 @@ async function getPerson(res, res, personUUID) {
   }
 }
 
-module.exports = {getPersons, getPerson};
+//@desc Create a person
+//@route POST/person
+async function createPerson(req, res) {
+  try {
+    let body = '';
+
+    req.on('data', (chunk) => {
+      console.log(chunk.toString());
+      body += chunk.toString();
+    });
+
+    req.on('end', async () => {
+      const {name, age, hobbies} = JSON.parse(body)
+
+      const person = {
+        name,
+        age,
+        hobbies,
+      };
+      
+      const newPerson = await Person.create(person);
+      res.writeHead(201, {'Content-Type':'application/json'});
+      res.end(JSON.stringify(newPerson));
+    });
+    
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+
+
+module.exports = {getPersons, getPerson, createPerson};
