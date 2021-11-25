98f6289f65556e7838e0f194996a2fb234c1f1eb
Svetlana Zhilina <sveta199951@gmail.com>
2021-11-26 00:03:39 +0300

    feat: add func create() for POST method

--- models/personModel.js
+++ models/personModel.js
@@ -1,10 +1,13 @@
-const persons = require('../data/person')
+const persons = require('../data/person');
+const {v4: uuidv4} = require('uuid');
+const  {writeDataToFile } = require('../utils');
 
 function findAll() {
   return new Promise((resolve, reject) => {
     resolve(persons);
   });
 }
+
 function findByUUID(uuid) {
   return new Promise((resolve, reject) => {
     const person = persons.find( p => p.id === uuid );
@@ -12,5 +15,15 @@ function findByUUID(uuid) {
   });
 }
 
+function create(person) {
+  return new Promise((resolve, reject) => {
+    const newPerson = {id: uuidv4(), ...person};
+    persons.push(newPerson);
+    writeDataToFile('./data/person.json', persons);
+    resolve(newPerson);
+  });
+}
 
-module.exports = { findAll, findByUUID };
+
+
+module.exports = { findAll, findByUUID, create };
